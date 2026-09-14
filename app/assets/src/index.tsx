99637e716632bd5f42f29aa44c4ff67f098e5a60
jsims-slower <jsims@slower.ai>
2026-09-14 14:34:15 -0600

    Bump Javascript Sentry version from 8.55.2 to 10.72.0 and enable Sentry consoleLogging integration.

--- app/assets/src/index.tsx
+++ app/assets/src/index.tsx
@@ -71,7 +71,20 @@ Sentry.init({
   environment: window.ENVIRONMENT,
   release: window.GIT_RELEASE_SHA,
   allowUrls: SEQTOID_ALLOWED_URLS,
-  beforeSend: event => (isGenerateFetchFnInfoNoise(event) ? null : event),
+  beforeSend: event => {
+    if (isGenerateFetchFnInfoNoise(event)) return null;
+    if (
+      event.level &&
+      (event.level === "log" ||
+        event.level === "info" ||
+        event.level === "debug")
+    )
+      return null;
+    return event;
+  },
+  integrations: [
+    Sentry.consoleLoggingIntegration({ levels: ["warn", "error", "assert"] }),
+  ],
 });
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
