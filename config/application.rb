23424b6fc5298059d0a86d88edc5b4df1211ac46
jsims-slower <jsims@slower.ai>
2026-09-17 14:10:05 -0600

    Remove ActiveRecord::LogSubscriber in preparation to adding JSON subscriber (TBD). Add better logging to elasticsearch_query_helper.rb

--- config/application.rb
+++ config/application.rb
@@ -73,12 +73,25 @@ module Czid
     config.host_authorization = { exclude: ->(request) { request.path == "/up" || request.path =~ /health_check/ || request.path.start_with?("/internal/") } }
     config.x.constants.default_background = 26
 
+    Rails.logger.info("Application.config.ctor: ActiveSupport::LogSubscriber.log_subscribers=#{ActiveSupport::LogSubscriber.log_subscribers&.inspect}")
+    Rails.logger.info("Application.config.ctor: ActiveRecord::LogSubscriber.log_subscribers=#{ActiveRecord::LogSubscriber.log_subscribers&.inspect}")
     config.after_initialize do
+      Rails.logger.info("Application.config.after_initialize: ActiveSupport::LogSubscriber.log_subscribers=#{ActiveSupport::LogSubscriber.log_subscribers&.inspect}")
+      Rails.logger.info("Application.config.after_initialize: ActiveRecord::LogSubscriber.log_subscribers=#{ActiveRecord::LogSubscriber.log_subscribers&.inspect}")
       ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
+        Rails.logger.info("Application.config.after_initialize: ActiveSupport.subscriber=#{subscriber&.inspect}")
         if subscriber.is_a?(ActiveRecord::LogSubscriber)
           ActiveSupport::LogSubscriber.detach_from(:active_record, subscriber)
         end
       end
+      ActiveRecord::LogSubscriber.log_subscribers.each do |subscriber|
+        Rails.logger.info("Application.config.after_initialize: ActiveRecord.subscriber=#{subscriber&.inspect}")
+        if subscriber.is_a?(ActiveRecord::LogSubscriber)
+          ActiveSupport::LogSubscriber.detach_from(:active_record, subscriber)
+        end
+      end
+      Rails.logger.info("Application.config.after_initialize.2: ActiveSupport::LogSubscriber.log_subscribers=#{ActiveSupport::LogSubscriber.log_subscribers&.inspect}")
+      Rails.logger.info("Application.config.after_initialize.2: ActiveRecord::LogSubscriber.log_subscribers=#{ActiveRecord::LogSubscriber.log_subscribers&.inspect}")
     end
   end
 end
